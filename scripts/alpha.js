ab212b114640c59871e53e8feec85b3deea986d5
muhammadkhayeralam <mohammadkhayeralam@gmail.com>
2024-02-15 17:26:58 +0600

    game over

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -17,11 +17,79 @@ function showInTheScreen(random){
  const alphabet=document.getElementById("showTheAlphabet");
  const allAlphabet="abcdefghigklmnopqrstuvwxyz";
  const arrayAlphabet=allAlphabet.split('');
- alphabet.innerText=arrayAlphabet[random];
+ alphabet.innerText=arrayAlphabet[random].toUpperCase();
  
  const keybd=document.getElementById(arrayAlphabet[random]);
  keybd.style.backgroundColor='gold';
 };
+
+
+
+function removeBackgroundById(identity){
+  const element=document.getElementById(identity);
+  console.log(element);
+  element.removeAttribute("style");
+};
+
+function gettingScore(identity){
+  const element=document.getElementById(identity);
+  const elementText=element.innerText;
+  const elementScore=parseInt(elementText);
+  return elementScore;
+}
+function updateTheScore(identity,newScore){
+  const element=document.getElementById(identity);
+ element.innerText=newScore;
+}
+
+function storeScore(newScore){
+  return newScore;
+}
+
+function handleKeyboardKeyUpEvent(event){
+  const playerPressed=event.key;
+  if(playerPressed==='Escape'){
+    gameOver();
+  }
+  const currentAlphabetElement=document.getElementById("showTheAlphabet");
+  const currentAlphabet=currentAlphabetElement.innerText;
+  const expectedAlphabet=currentAlphabet.toLowerCase();
+  console.log(playerPressed,expectedAlphabet);
+  if(expectedAlphabet===playerPressed)
+  {
+    console.log("you win ");
+    // scrore update
+    // const currentScoreElement=document.getElementById("current-score");
+    // const currentScoreText=currentScoreElement.innerText;
+    // const currentScore=parseInt(currentScoreText);
+    const currentScore=gettingScore("current-score");
+    const newScore=currentScore+1;
+    const score=storeScore(newScore);
+    // currentScoreElement.innerText=newScore;
+    updateTheScore("current-score",newScore)
+    // score update ended
+    const random=getRandomAlphabet();
+    removeBackgroundById(expectedAlphabet);
+  showInTheScreen(random);  
+  }
+  else{
+    console.log("you lost and lost a life");
+    // const currentLifeElement=document.getElementById("current-life");
+    // const currentLifeText=currentLifeElement.innerText;
+    // const currentLife=parseInt(currentLifeText);
+    const currentLife=gettingScore("current-life")
+    const newLife=currentLife-1;
+    // currentLifeElement.innerText=newLife;
+    updateTheScore("current-life",newLife);
+    if(newLife===0){
+      gameOver();
+    }
+
+  }
+  // showInTheScreen();
+}
+document.addEventListener('keyup',handleKeyboardKeyUpEvent);
+
 
 function playNow(){
   hideElementById("section-1");
@@ -29,4 +97,22 @@ function playNow(){
   const random=getRandomAlphabet();
   showInTheScreen(random);
 
+}
+
+function gameOver(){
+  hideElementById("section-2");
+  visibleElementById("section-3");
+  const scoreBoardElement=document.getElementById("score-board");
+  scoreBoardElement.innerText=document.getElementById("current-score").innerText;
+  // const scoreBoard=gettingScore("score-board");
+  // updateTheScore
+}
+
+function playAgain(){
+  hideElementById("section-3");
+  visibleElementById("section-2");
+  const newLife=5;
+  updateTheScore("current-life",newLife);
+  const newScore=0;
+  updateTheScore("current-score",newScore);
 }
